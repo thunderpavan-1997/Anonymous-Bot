1ef177b0c32903fee6e8a7dac8539dd48db15813
thunderpavan-1997 <57397056+thunderpavan-1997@users.noreply.github.com>
2021-02-28 14:59:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from vars import var
 logging.basicConfig(level=logging.INFO)
 
 AnonyBot = Client('Anonymous-Sender',
-                  api_id=var.API_ID,
-                  api_hash=var.API_HASH,
                   bot_token=var.BOT_TOKEN,
                   plugins=dict(root="plugins"))
 
